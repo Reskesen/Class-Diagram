91ec788ff651d024d7c8b35ebccd4a9a970aa5c2
Jacob Thinglev Grundahl <jacob.grundahl@gmail.com>
2015-10-27 13:08:27 +0100

    Structure setup and you can now RUN IT!!!!! XD

--- ClassDiagram/ClassDiagram/View/App.xaml.cs
+++ ClassDiagram/ClassDiagram/View/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ClassDiagram
+namespace ClassDiagram.View
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- ClassDiagram/ClassDiagram/View/MainWindow.xaml.cs
+++ ClassDiagram/ClassDiagram/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ClassDiagram
+namespace ClassDiagram.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
