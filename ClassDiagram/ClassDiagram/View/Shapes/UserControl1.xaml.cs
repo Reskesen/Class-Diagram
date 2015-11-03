e865fb8d354ab8a321a5c9cf14497805315f809e
Henrik <seje-smith@hotmail.com>
2015-11-03 09:50:23 +0100

    Changed names in Shapes

--- ClassDiagram/ClassDiagram/View/Shapes/UserControl1.xaml.cs
+++ ClassDiagram/ClassDiagram/View/Shapes/UserControl1.xaml.cs
@@ -18,9 +18,9 @@ namespace ClassDiagram.View.Shapes
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class ClassShape : UserControl
     {
-        public UserControl1()
+        public ClassShape()
         {
             InitializeComponent();
         }
