5619ac4389b23e821e59f5a02a3ae53d44933f62
Jacob Thinglev Grundahl <jacob.grundahl@gmail.com>
2015-11-03 11:45:16 +0100

    mouse move shape..../addline

---
+++ ClassDiagram/ClassDiagram/ClassShape.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ClassDiagram.Model
+{
+
+    public class ClassShape : NotifyBase
+    {
+        private static int counter = 0;
+
+        public int Number { get; }
+
+        private double x = 200;
+
+        public double X { get { return x; } set { x = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterX); } }
+
+        private double y = 200;
+
+        public double Y { get { return y; } set { y = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterY); } }
+
+        private double width = 100;
+
+        public double Width { get { return width; } set { width = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterX); NotifyPropertyChanged(() => CenterX); } }
+
+        private double height = 100;
+
+        public double Height { get { return height; } set { height = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterY); NotifyPropertyChanged(() => CenterY); } }
+
+        public double CanvasCenterX { get { return X + Width / 2; } set { X = value - Width / 2; NotifyPropertyChanged(() => X); } }
+
+        public double CanvasCenterY { get { return Y + Height / 2; } set { Y = value - Height / 2; NotifyPropertyChanged(() => Y); } }
+
+        public double CenterX => Width / 2;
+
+        public double CenterY => Height / 2;
+
+        // ViewModel properties.
+        // These properties should be in the ViewModel layer, but it is easier for the demo to put them here, 
+        //  to avoid unnecessary complexity.
+        // NOTE: This breaks the seperation of layers of the MVVM architecture pattern.
+        //       To avoid this a ShapeViewModel class should be created that wraps all Shape objects, 
+        //        but it adds to the complexity of the ViewModel layer and this demo and a simpler solution was chosen for the demo.
+        //        (this also adds a reference to the PresentationCore class library which is part of .NET, 
+        //         but should not be used in the Model layer, creating an unnecessary dependency for the Model layer class library).
+        //       To learn how to avoid this and create an application with a more pure MVVM architecture pattern, 
+        //        please ask the Teaching Assistants.
+        private bool isSelected;
+        public bool IsSelected { get { return isSelected; } set { isSelected = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => SelectedColor); } }
+        public Brush SelectedColor => IsSelected ? Brushes.Red : Brushes.Yellow;
+        public ClassShape()
+        {
+            Number = ++counter;
+        }
+        public override string ToString() => Number.ToString();
+    }
+}
--- ClassDiagram/ClassDiagram/View Model/MainViewModel.cs
+++ ClassDiagram/ClassDiagram/View Model/MainViewModel.cs
@@ -1,12 +1,130 @@
-﻿using System;
+﻿using ClassDiagram.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace ClassDiagram.View_Model
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        private Boolean isAddingLine;
+        private ClassShape addingLineFrom;
+
+        private Point initialMousePosition;
+        private Point initialShapePosition;
+
+        public MainViewModel()
+        {
+
+        }
+
+
+
+
+        private void MouseDownShape(MouseButtonEventArgs e)
+        {
+            //if not adding a line
+            if (!isAddingLine)
+            {
+                var shape = TargetShape(e);
+               
+                var mousePosition = RelativeMousePosition(e);
+
+                initialMousePosition = mousePosition;
+                initialShapePosition = new Point(shape.X, shape.Y);
+
+                e.MouseDevice.Target.CaptureMouse();
+            }
+        }
+
+        private void MouseMoveShape(MouseEventArgs e)
+        {
+            //if mouse captured and not adding a line
+            if (Mouse.Captured != null && !isAddingLine)
+            {
+                var shape = TargetShape(e);
+             
+                var mousePosition = RelativeMousePosition(e);
+
+                //shape moved by offset
+                shape.X = initialShapePosition.X + (mousePosition.X - initialMousePosition.X);
+                shape.Y = initialShapePosition.Y + (mousePosition.Y - initialMousePosition.Y);
+            }
+        }
+
+        private void MouseUpShape(MouseButtonEventArgs e)
+        {
+            //addingline
+            if (isAddingLine)
+            {
+                ////retrieves selected target
+                //var shape = TargetShape(e);
+
+                ////if no shape has been selected
+                //if (addingLineFrom == null)
+                //{
+                //    addingLineFrom = shape;
+                //    addingLineFrom.IsSelected = true;
+                //}
+                ////  if the two shapes aren't the same
+                //else if (addingLineFrom.Number != shape.Number)
+                //{
+                //    undoRedoController.AddAndExecute(new AddLineCommand(Lines, new Line() { From = addingLineFrom, To = shape }));
+
+                //    addingLineFrom.IsSelected = false;
+
+                //    isAddingLine = false;
+                //    addingLineFrom = null;
+
+                //    RaisePropertyChanged(() => ModeOpacity);
+                //}
+            }
+            //moving a Shape.
+            else
+            {
+                // shape is retrieved
+                var shape = TargetShape(e);
+                // The mouse position relative to the target of the mouse event.
+                var mousePosition = RelativeMousePosition(e);
+
+                // The Shape is moved back to its original position, so the offset given to the move command works.
+                shape.X = initialShapePosition.X;
+                shape.Y = initialShapePosition.Y;
+
+                
+        //        undoRedoController.AddAndExecute(new MoveShapeCommand(shape, mousePosition.X - initialMousePosition.X, mousePosition.Y - initialMousePosition.Y));
+
+                // The mouse is released
+                e.MouseDevice.Target.ReleaseMouseCapture();
+            }
+        }
+
+        private ClassShape TargetShape(MouseEventArgs e)
+        {
+            var shapeVisualElement = (FrameworkElement)e.MouseDevice.Target;
+
+            return (ClassShape)shapeVisualElement.DataContext;
+        }
+
+        private Point RelativeMousePosition(MouseEventArgs e)
+        {
+            var shapeVisualElement = (FrameworkElement)e.MouseDevice.Target;
+
+            var canvas = FindParentOfType<Canvas>(shapeVisualElement);
+
+            return Mouse.GetPosition(canvas);
+        }
+
+        private static T FindParentOfType<T>(DependencyObject o)
+        {
+            dynamic parent = VisualTreeHelper.GetParent(o);
+            return parent.GetType().IsAssignableFrom(typeof(T)) ? parent : FindParentOfType<T>(parent);
+        }
     }
 }
