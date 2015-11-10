c3b07daefbad8ba68528cce49079d34e3638e6f2
Reskesen <Reskesen@gmail.com>
2015-11-10 11:56:18 +0100

    Line works completely now!!

--- ClassDiagram/ClassDiagram.Model/Line.cs
+++ ClassDiagram/ClassDiagram.Model/Line.cs
@@ -11,10 +11,10 @@ namespace ClassDiagram.Model
     public class Line : NotifyBase
     {
 
-        private ClassShape from;
+        public ClassShape from;
         public ClassShape From { get { return from; } set { from = value; NotifyPropertyChanged(); } }
 
-        private ClassShape to;
+        public ClassShape to;
         public ClassShape To { get { return to; } set { to = value; NotifyPropertyChanged(); } }
     }
 }
